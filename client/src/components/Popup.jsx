45b2f24640f0d309ddfe24cb47d04db6d53594be
P.Prajith <129357819+Prajith0765@users.noreply.github.com>
2024-03-18 23:18:19 +0530

    modified:   client/src/components/Popup.jsx modified:   client/src/components/Rollno.jsx

--- client/src/components/Popup.jsx
+++ client/src/components/Popup.jsx
@@ -30,14 +30,17 @@ export default function Popup() {
                 <div className="absolute bottom-0 right-0 m-4">
                     <button className="absolute top-0 right-0" onClick={togglePopup}>X</button>
                     <div className="bg-white shadow-md rounded-lg p-4 w-96 h-96 flex flex-col justify-center items-center border border-gray-390">
-                        <div className="mb-9">
-                            <h1 className="text-lg font-bold">Are you a Parent or Student?</h1>
-                        </div>
+                        
                         {!isParentClicked && !isStudentClicked && (
-                            <div className="flex justify-between">
+                            
+    
+                            <div className="mb-9">
+                                <h1 className="text-lg font-bold mb-9">Are you a Parent or Student?</h1>
+                                <div className="flex justify-between">
                                 <button className="bg-orange-500 hover:bg-blue-600 text-white font-bold py-4 px-9 rounded" onClick={handleParentButtonClick}>Parent</button>
                                 <div className="mx-4"></div>
                                 <button className="bg-orange-500 hover:bg-blue-600 text-white font-bold py-2 px-9 rounded" onClick={handleStudentButtonClick}>Student</button>
+                                </div>
                             </div>
                         )}
                         
