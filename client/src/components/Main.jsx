d9d5a85e1b576289a87141d437981b6cef1bada7
P.Prajith <129357819+Prajith0765@users.noreply.github.com>
2024-03-19 14:00:37 +0530

    modified:   client/src/components/Main.jsx 	modified:   client/src/components/Rollno.jsx

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -3,13 +3,14 @@ export default function Main(){
 
     return(
         <div>
-            <div>
-                <button className="">Marks</button>
-                <button className="">Attendance</button>
-                <button className="">Academic Performance</button>
-                <button className="">Absent</button>
-
+            <div className="flex flex-wrap justify-center gap-5">
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-5 h-20 w-36">Marks</button>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded h-20 w-36">Attendance</button>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-5 h-20 w-36">Academic Performance</button>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded  h-20 w-36">Absent</button>
+                <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded h-10 w-30 mt-5">Back</button>
             </div>
+
         </div>
     );
 }
--- client/src/components/Rollno.jsx
+++ client/src/components/Rollno.jsx
@@ -1,6 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import { useState } from "react";
 import Popup from "./Popup";
+import Main from "./Main";
 
 export default function Rollno(){
     const [isBackClicked, setIsBackClicked] = useState(false);
@@ -21,12 +22,14 @@ export default function Rollno(){
         <div className="mb-4">
                 <p className="text-lg font-semibold">Enter Your Roll Number</p>
                 <input type="text" className="border border-gray-300 rounded-md px-4 py-2 w-full mt-2 focus:outline-none focus:border-blue-500"></input>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleSubmitCLciked}>Submit</button>
+                <button className="bg-orange-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleSubmitCLciked}>Submit</button>
                 <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded mt-4 ml-2" onClick={handleBackClciked}>Back</button>
         </div>
              )}
         {isBackClicked && <Popup/>}                     
-        {isSubmitClicked && <Popup />}
+        
+        {isSubmitClicked && <Main/>}
+                        
         </div>
     );
 }
