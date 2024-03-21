cd79f2722d5c33de1e79e850312952cb19e44919
P.Prajith <129357819+Prajith0765@users.noreply.github.com>
2024-03-21 16:22:34 +0530

    modified:   client/src/components/Main.jsx

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -7,7 +7,7 @@ export default function Main(){
                 <button className="bg-orange-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-5 h-20 w-36">Marks</button>
                 <button className="bg-orange-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded h-20 w-36">Attendance</button>
                 <button className="bg-orange-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-5 h-20 w-36">Academic Performance</button>
-                <button className="bg-orange-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded  h-20 w-36">Absent</button>
+                <button className="bg-orange-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded  h-20 w-36">Co-activities</button>
                 <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded h-10 w-30 mt-5">Back</button>
             </div>
             <div className="flex justify-center items-end mt-5">
