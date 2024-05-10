1460e32ceb03fba269e25b628c05d991b5552c95
P.Prajith <129357819+Prajith0765@users.noreply.github.com>
2024-05-10 07:20:06 +0530

    modified:   client/src/components/OTPverify.jsx

--- client/src/components/OTPverify.jsx
+++ client/src/components/OTPverify.jsx
@@ -70,7 +70,7 @@ export default function OTPverify() {
                                 onChange={(ev) => setOTP(ev.target.value)}
                                 className="border border-gray-300 rounded-md px-4 py-2 w-full mt-2 focus:outline-none focus:border-blue-500"
                             />
-                            <button type="submit">Submit</button>
+                            
                         </form>
                     </div>
 
